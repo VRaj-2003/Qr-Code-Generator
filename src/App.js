2bef260fe1394acca0a5e75c577cf40619bbf569
Vaibhav Raj <vaibhavraj6204064029@gmail.com>
2024-10-09 17:03:25 +0530

    Qr Code Generator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import "./App.css";
+import { Button } from 'primereact/button';
+import { InputTextarea } from "primereact/inputtextarea";
+import { useState } from "react";
+import "primereact/resources/themes/bootstrap4-light-purple/theme.css";
+import QRCode from 'qrcode';
+import { Card } from "primereact/card";
 
 function App() {
+  // User input
+  // Generate the code
+  // download the Qr
+
+  const [query, setQuery] = useState("");
+  const [qrUrl, setQrUrl] = useState("");
+
+  const generateQrCode = async () => {
+    try {
+      const dataUrl = await QRCode.toDataURL(query);
+      setQrUrl(dataUrl);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+    const downloadQrCode = () => {
+      try {
+        const link = document.createElement('a')
+        link.href = qrUrl
+        link.download = encodeURIComponent('qr code')
+        link.style.display = 'none'
+        link.click()
+        document.removeChild(link)
+      } catch (e) {
+        alert("Download Sucessfully")
+      }
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>QR Code Generator</h1>
+      <InputTextarea
+        autoResize
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        rows={5}
+        cols={30}
+      />
+      <br />
+     
+      <Button label="Generate QR Code" onClick={generateQrCode} />
+
+      {
+        qrUrl.length ? (
+          <>
+            <Card title="QR Code" style={{minWidth: '20vw', width: 'fit-content', margin: '10vh auto'}}>
+              <img src = {qrUrl} alt="qrcode" width={300}></img>
+              <br/>
+              <Button label="Download" onClick={downloadQrCode} />
+            </Card>
+          </>
+        ) : " "
+      }
     </div>
   );
 }
+
 
 export default App;
